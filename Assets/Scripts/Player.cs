a7acf7d4fb8fea3840d8c447a71a30ca44f8a929
tgartside <128266784+tgartside@users.noreply.github.com>
2025-08-22 19:30:31 -0700

    Added basic movement functionality
    Player can now move with WASD and can dash with Space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,18 @@ public class Player : MonoBehaviour
     public Transform jumpCheck;
     public GameObject sparkle;
 
+    [Header("Movement Variables")]
+    public float moveSpeed;
+    private Vector2 movement;
+
+    [Header("Dash Variables")]
+    public bool dashPressed = false;
+    public bool canDash = true;
+    public bool isDashing = false;
+    public float dashStrength;
+    public float dashTime;
+    public float dashCooldown;
+
     bool dead = false;
 
     void Start()
@@ -23,14 +35,36 @@ public class Player : MonoBehaviour
     {
         if (!dead)
         {
+            if (isDashing)
+            {
+                return;
+            }
 
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            movement = Vector2.ClampMagnitude(movement, 1);
+            rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+
+            if (dashPressed)
+            {
+                StartCoroutine(Dash());
+                dashPressed = false;
+            }
 
         }
     }
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            dashPressed = true;
+        }
     }
 
 
@@ -52,5 +86,16 @@ public class Player : MonoBehaviour
     public bool isDead()
     {
         return dead;
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(rb.velocity.x * dashStrength, rb.velocity.y * dashStrength);
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
