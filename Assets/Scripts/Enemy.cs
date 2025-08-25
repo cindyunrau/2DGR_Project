cf883c9fd58ad40fcced25f14510dd8f85d3307f
tgartside <128266784+tgartside@users.noreply.github.com>
2025-08-25 16:29:16 -0700

    Updated enemy pathfinding
    Added a NavMesh that enemies now use to navigate

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
 
-    [SerializeField] private Vector2 target;
-    [SerializeField] private GameObject player;
-    [SerializeField] private float moveSpeed = 2f;
-    private float distance;
+    //[SerializeField] private Vector2 target;
+    //[SerializeField] private GameObject player;
+    //[SerializeField] private float moveSpeed = 2f;
+    //private float distance;
+
+    [SerializeField] private Transform target;
+    NavMeshAgent agent;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        //target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Awake()
+    {
+        target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -25,11 +38,12 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        target = player.transform.position;
-        distance = Vector2.Distance(transform.position, target);
-        if (distance > 1)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
-        }
+        //target = player.transform.position;
+        //distance = Vector2.Distance(transform.position, target);
+        //if (distance > 1)
+        //{
+        //    transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        //}
+        agent.SetDestination(target.position);
     }
 }
