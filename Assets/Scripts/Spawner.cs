4186f1df92a43cfc7d29381884031fbe939514a7
tgartside <128266784+tgartside@users.noreply.github.com>
2025-08-25 20:14:30 -0700

    Enemies can overlap again
    Enemies can now overlap again, hopefully preventing them from pushing the player through walls. Enemies will have to spawn at fixed points for this to work

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,10 +17,10 @@ public class Spawner : MonoBehaviour
             if (child.nextTimeToSpawn <= Time.time)
             {
                 // Get a random position in a circle around the player
-                float randomAngle = Random.Range(0f, 360f);
-                float spawnX = player.transform.position.x + spawnRadius * Mathf.Cos(randomAngle * Mathf.Deg2Rad);
-                float spawnY = player.transform.position.y + spawnRadius * Mathf.Sin(randomAngle * Mathf.Deg2Rad);
-                child.transform.position = new Vector2 (spawnX, spawnY);
+                //float randomAngle = Random.Range(0f, 360f);
+                //float spawnX = player.transform.position.x + spawnRadius * Mathf.Cos(randomAngle * Mathf.Deg2Rad);
+                //float spawnY = player.transform.position.y + spawnRadius * Mathf.Sin(randomAngle * Mathf.Deg2Rad);
+                //child.transform.position = new Vector2 (spawnX, spawnY);
 
                 // Spawn enemy at that location
                 SpawnEntity(child);
